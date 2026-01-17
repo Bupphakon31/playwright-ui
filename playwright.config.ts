146a563721c83575705543c7cf64acf7dd8a6a8a
Bupphakon31 <bupphakon.nak31@gmail.com>
2026-01-17 15:43:25 +0700

    edit config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './testCases',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
